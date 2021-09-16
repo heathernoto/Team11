e9efb835eaabd8ea8cf78e130b7df13c6d62f231
heathernoto <heather.noto@gmail.com>
2021-09-16 17:05:39 -0400

    added code for search bar- not reworked

--- frontend/components/SearchBar.jsx
+++ frontend/components/SearchBar.jsx
@@ -1,9 +1,36 @@
 import React from "react";
+import {InputGroup, Dropdown, SplitButton, FormControl} from "react-bootstrap";
 
-const SearchBar = (props) => {
-    return (<div>
-        Search
-    </div>)
+class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+
+      }
+
+
+
+
+    render(){
+        return (
+    <InputGroup className="mb-3">
+        <FormControl aria-label="Text input with dropdown button" />
+            <SplitButton
+                variant="outline-secondary"
+                title="Action"
+                id="segmented-button-dropdown-2"
+                alignRight
+            >
+            <Dropdown.Item href="#">Action</Dropdown.Item>
+            <Dropdown.Item href="#">Another action</Dropdown.Item>
+            <Dropdown.Item href="#">Something else here</Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item href="#">Separated link</Dropdown.Item>
+            </SplitButton>
+  </InputGroup>
+        )
+
+    }
+
 }
 
 export default SearchBar
