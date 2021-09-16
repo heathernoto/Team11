d143009f1edfe8e17eb7da6ba378365c68945f35
Raymond <raymondcoolmail@gmail.com>
2021-09-16 13:51:20 -0400

    added home controller. added entity

--- src/main/java/com/jpmchase/Team11Application.java
+++ src/main/java/com/jpmchase/Team11Application.java
@@ -3,11 +3,11 @@ package com.jpmchase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class DemoApplication {
+public class Team11Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(Team11Application.class, args);
 	}
-
 }
--- src/test/java/Team11ApplicationTests.java
+++ src/test/java/Team11ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class Team11ApplicationTests {
 
 	@Test
 	void contextLoads() {
